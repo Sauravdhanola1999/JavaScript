1221fc868c86b3a017f85595aa4902234bae4b9a
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-01-23 14:05:48 +0530

    add javascript.js with type checks and equality examples

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// Types in Js
+console.log(typeof "hello");   // "string"
+console.log(typeof 42);        // "number"
+console.log(typeof true);      // "boolean"
+console.log(typeof undefined); // "undefined"
+console.log(typeof null);      // "object" (special case)
+console.log(typeof {});        // "object"
+console.log(typeof []);        // "object"
+console.log(typeof function(){}); // "function"
+
+
+// equality and strict equality
+console.log(5=='5');
+console.log(5==='5');
+
+//null and undefined are both represent the absence of a value
+
+
