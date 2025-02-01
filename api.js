c9a65b443fa808b18e54e1377e7ad24bddb550dc
Saurav Singh Dhanola <sauravsingh16101999@gmail.com>
2025-02-01 10:32:15 +0530

    array methods

--- api.js
+++ api.js
@@ -5,27 +5,59 @@ let students = [
   { name: "Anything", rollNumber: 7, marks: 90 },
 ];
 
-let newStudent = students.map((student)=>{
-    return student.name.toUpperCase();
-})
+let newStudent = students.map((student) => {
+  return student.name.toUpperCase();
+});
 // console.log(newStudent);
 
-let newStudent2 = students.filter(student => student.marks > 60 && student.rollNumber >15);
+let newStudent2 = students.filter(
+  (student) => student.marks > 60 && student.rollNumber > 15
+);
 // console.log(newStudent2);
 
-let sumofallmarks = students.reduce((acc, student)=> acc + student.marks, 0);
+let sumofallmarks = students.reduce((acc, student) => acc + student.marks, 0);
 // console.log(sumofallmarks);
 
-let students2 = students.filter(student => student.marks > 60 ).map(student => student.name);
+let students2 = students
+  .filter((student) => student.marks > 60)
+  .map((student) => student.name);
 // console.log(students2);
 
-let totalMarks = students.map(student =>{
-  if(student.marks < 60){
-    student.marks += 20;
+let totalMarks = students
+  .map((student) => {
+    if (student.marks < 60) {
+      student.marks += 20;
+    }
+    return student;
+  })
+  .filter((student) => student.marks > 60)
+  .reduce((acc, student) => acc + student.marks, 0);
+// console.log(totalMarks);
+
+const users = [
+  { firstName: "Saurav", lastName: "Singh", age: 26 },
+  { firstName: "anabella", lastName: "Higher", age: 45 },
+  { firstName: "godsake", lastName: "Another", age: 57 },
+  { firstName: "Sulekha", lastName: "Upper", age: 26 },
+];
+
+let fullName = users.map((user) => `${user.firstName} ${user.lastName}`);
+// console.log(fullName);
+
+let reduceoutput = users.reduce((acc, current) => {
+  if (acc[current.age]) {
+    acc[current.age] = ++acc[current.age];
+  } else {
+    acc[current.age] = 1;
   }
-  return student;
-}).filter(student => student.marks > 60).reduce((acc, student) => acc + student.marks, 0);
+  return acc;
+}, {});
 
-console.log(totalMarks);
+// console.log(reduceoutput);
 
 
+const firstnameusers = users.filter((user) => user.age < 30).map(
+  (user) => user.firstName
+);
+
+console.log(firstnameusers);
